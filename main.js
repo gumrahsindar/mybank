0ca4b4d228f0f8859e64d781782349d74e311c4e
gumrahsindar <gsindar@gmail.com>
2024-09-27 18:02:02 +0300

    Refactor slider functionality and add dot navigation

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const slider = document.querySelector('#slider')
+const dots = document.querySelectorAll('#dots .dot')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let lastIndex = 0
+slider.addEventListener(
+  'scroll',
+  () => {
+    const index = Math.round(slider.scrollLeft / window.innerWidth)
+    if (index === lastIndex) {
+      return false
+    }
+    lastIndex = index
+    document.querySelector('#dots .dot.active')?.classList.remove('active')
+    dots[index].classList.add('active')
+  },
+  { passive: true }
+)
